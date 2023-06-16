b621cf6159c993b23d687d8d6ef04bcc8fe491ad
Daniel Zinn <62887535+11sdz@users.noreply.github.com>
2023-06-16 20:37:04 +0300

    Add files via upload

--- chardev.c
+++ chardev.c
@@ -25,20 +25,20 @@ static long int device_ioctl(struct file *,unsigned int ioctl_num , unsigned lon
 
 #define SUCCESS 0
 #define DEVICE_NAME "chardev"
-#define BUFFER 64
-#define IOCTL_CHANGEKEY _IOW('n' ,1,int)
+#define BUFFER 64 // BUFFER SIZE
+#define IOCTL_CHANGEKEY _IOW('n' ,1,int) //ioctl cmd for case we get new key
 
 static int major; // major  number assigned to our device
-volatile static int is_open=0;
-static int write_offset=0;
+volatile static int is_open=0; // if we opened device
+static int write_offset=0; //where to write on the next write call
 
 
-static int key_pos=0;
-static char message[BUFFER];
-static unsigned int buffer_size=0;
+static int key_pos=0; //for our encryption procedure
+static char message[BUFFER]; //where our data is located
+static unsigned int buffer_size=0; // length of message how many bytes writed in BUFFER
 
-static int key=0;
-module_param(key, int , 0000); 
+static int key=0;// key to encrypt data message[0-4] xor key because key is 4 bytes
+module_param(key, int , 0000); //decleration so we can assign on insmod chardev.ko key=(some value)
 MODULE_PARM_DESC(key, "An integer");
 
 static struct class *cls;
@@ -48,6 +48,8 @@ static struct class *cls;
 // static struct kobj_attribute bytes_registered_attr = 
 //                     __ATTR(bytes_registered, 0660, bytes_registered_show, NULL); 
 
+
+//file operations struct we assign each method
 static struct file_operations chardev_fops = { 
     .owner = THIS_MODULE,
     .read = device_read, 
@@ -57,7 +59,7 @@ static struct file_operations chardev_fops = {
     .unlocked_ioctl = device_ioctl,
 
 }; 
-
+//initilaize
 static int __init chardev_init(void){
     printk(KERN_INFO "(chardev init)\n");
     memset(message,0,BUFFER);
@@ -109,20 +111,22 @@ static int device_release(struct inode * iNode, struct file * myFile){
     module_put(THIS_MODULE);
     return SUCCESS; 
 }
+
+//read operation
 static ssize_t device_read(struct file * myFile, char __user * buffer, size_t sizeT, loff_t * offset){
-    int bytes_read=0;
+    int bytes_read=0;//how many bytes we read
     int read_offset=0;
-    unsigned char* bytes_key =(unsigned char*)&key;
+    unsigned char* bytes_key =(unsigned char*)&key;// casting our key to 4 bytes each byte will be represented as char
     size_t keylen=strlen(bytes_key);
     if(offset ==NULL){
         return -1;
     }
 
     while((bytes_read<BUFFER) && (*offset < buffer_size)){
-        key_pos=read_offset % keylen;
-        message[read_offset]=message[read_offset]^bytes_key[key_pos];
-        put_user(message[read_offset],&buffer[*offset]);
-        message[read_offset]=message[read_offset]^bytes_key[key_pos];
+        key_pos=read_offset % keylen;//encryption using each one of the bytes 0,1,2,3 .. 4 bytes
+        message[read_offset]=message[read_offset]^bytes_key[key_pos];//decrypt
+        put_user(message[read_offset],&buffer[*offset]);//printing on user output
+        message[read_offset]=message[read_offset]^bytes_key[key_pos];//encrypt
         bytes_read++;
         read_offset++;
         *offset=*offset+1;
@@ -150,7 +154,7 @@ static ssize_t device_write(struct file *myFile, const char __user * buffer, siz
     while(bytes_write< sizeT){
         get_user(message[write_offset], &buffer[bytes_write]);
         key_pos=write_offset % keylen;
-        message[write_offset]=message[write_offset]^bytes_key[key_pos];
+        message[write_offset]=message[write_offset]^bytes_key[key_pos];//same as read encrypted here
         bytes_write++;
         write_offset++;
         if(write_offset>=BUFFER-1){
@@ -165,9 +169,9 @@ static ssize_t device_write(struct file *myFile, const char __user * buffer, siz
     return bytes_write;
 }
 static long int device_ioctl(struct file *,unsigned int ioctl_num , unsigned long ioctl_param){
-    int newkey=0;
-    int keylen=0;
-    int idx=0,keypos=0;
+    int newkey=0;//for the new key we get from mioctl
+    int keylen=0;//len of key 4bytes
+    int idx=0,keypos=0;//same for read
     unsigned char* bytes_key =NULL;
     switch(ioctl_num){
         case IOCTL_CHANGEKEY:
@@ -175,16 +179,18 @@ static long int device_ioctl(struct file *,unsigned int ioctl_num , unsigned lon
                 return -EFAULT;
             }
             if(key==newkey){
+                //same key data is already encrypted
                 break;
             }
             printk(KERN_INFO "new key %d\n",newkey);
+            //decrypt using old key
             bytes_key = (unsigned char*)&key;
             keylen= strlen(bytes_key);
             for(idx=0; idx<buffer_size;idx++){
                 keypos= idx % keylen;
                 message[idx]=message[idx]^bytes_key[keypos];
             }
-
+            //encrypt using new key
             key=newkey;
             bytes_key = (unsigned char*)&key;
             keylen= strlen(bytes_key);
